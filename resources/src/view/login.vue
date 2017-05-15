3a1dce6b47e703da75076cc5228ecf3d87825a25
dudeyouth <1157536217@qq.com>
2017-05-15 15:22:51 +0000

    login

--- resources/src/view/login.vue
+++ resources/src/view/login.vue
@@ -4,30 +4,21 @@
 			<el-col :span="24"><div class="top-title">欢迎使用 WEB-CRONTAB</div></el-col>
 		</el-row>
 
-		<el-form ref="form" :model="form" label-width="80px">
-			<el-form-item label="用户名称">
-				<el-input v-model="form.username"></el-input>
+		<el-form ref="ruleForm" :rules="rules" :model="ruleForm" label-width="80px">
+			<el-form-item label="用户名称" prop="username">
+				<el-input v-model="ruleForm.username"></el-input>
 			</el-form-item>
-			<el-form-item label="密码">
-				<el-input v-model="form.password"></el-input>
+			<el-form-item label="密码" prop="password">
+				<el-input v-model="ruleForm.password"></el-input>
 			</el-form-item>
-			<el-form-item label="记住密码">
-				<el-switch on-text="" off-text="" v-model="form.delivery"></el-switch>
+			<el-form-item label="记住密码" prop="delivery">
+				<el-switch on-text="" off-text="" v-model="ruleForm.delivery"></el-switch>
 			</el-form-item>
 			<el-form-item>
-				<el-button type="primary" @click="onSubmit">登陆</el-button>
+				<el-button type="primary" @click="submitForm('ruleForm')">登陆</el-button>
 				<el-button>取消</el-button>
 			</el-form-item>
 		</el-form>
-
-		<el-dialog title="提示" v-model="dialogVisible" size="tiny" :before-close="handleClose">
-		<span>账号密码错误</span>
-		<span slot="footer" class="dialog-footer">
-			<el-button @click="dialogVisible = false">取 消</el-button>
-			<el-button type="primary" @click="dialogVisible = false">确 定</el-button>
-		</span>
-	</el-dialog>
-
 </div>
 </template>
 
@@ -38,35 +29,54 @@
 		name: 'page',
 		data () {
 			return {
-				form: {
+				ruleForm: {
 					username: '',
 					password: '',
 					delivery: false,
 				},
 				dialogVisible: false,
+				rules: {
+				  username: [
+					{ required: true, message: '请输入用户名', trigger: 'blur' },
+					{ min: 3, max: 20, message: '长度在 3 到 20 个字符', trigger: 'blur' },
+				  ],
+				  password: [
+					{ required: true, message: '请输入密码', trigger: 'blur' },
+					{ min: 4, max: 20, message: '长度在 4 到 20 个字符', trigger: 'blur' }
+				  ]
+				},
 			}
 		},
 		methods: {
-			onSubmit() {
-				var formData = this.form; // 表单数据
-				var paren   = this;
+			submitForm(formName) {
+				var formData = this.ruleForm; // 表单数据
+				var _this   = this;
 				var qs = require('qs');
-
-				http.post('/login', qs.stringify({
-					username: formData.username,
-					password: formData.password,
-					delivery: formData.delivery,
-				}))
-				.then(function (response) {
-					if(response.data.statusCode==10001){
-						VueCookie.set('Authorization', response.data.arrData.Authorization,24*3600);						
-						console.log(response.data.arrData.Authorization);
-						goto('/home');
-					}else{
-						console.log('login false');
-						paren.dialogVisible = true;
-					}
-					
+				this.$refs[formName].validate((valid) => {
+				  if (valid) {
+					http.post('/login', qs.stringify({
+						username: formData.username,
+						password: formData.password,
+						delivery: formData.delivery,
+					}))
+					.then(function (response) {
+						if(response.data.statusCode==10001){
+							VueCookie.set('Authorization', response.data.arrData.Authorization,24*3600);						
+							console.log(response.data.arrData.Authorization);
+							router.push('/index');
+						}else{
+							console.log('login false');
+							_this.$message({type: 'warning',showClose: true,'message':'账号密码错误！'});
+						}
+						
+					})
+					.catch(()=>{
+						_this.$message({type: 'error',showClose: true,'message':'网络错误！'});
+					})
+				  } else {
+					console.log('error submit!!');
+					return false;
+				  }
 				});
 				
 			},
