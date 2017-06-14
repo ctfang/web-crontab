447969de00fa32077e266a4d120a24773c4c8a70
git <1157536217@qq.com>
2017-06-14 17:22:29 +0800

    test

--- resources/src/components/complete.vue
+++ resources/src/components/complete.vue
@@ -3,12 +3,12 @@
         
         <el-row style="margin-top:30px;text-align:center">
             <el-row>
-                <el-col :span='12'>
-                    <el-button>默认按钮</el-button>
-                </el-col>
-                <el-col :span='12'>
-                    <el-button>默认按钮</el-button>
-                </el-col>
+                    <el-col :span='12'>
+                        <el-button>2017-05-05</el-button>
+                    </el-col>
+                    <el-col :span='12'>
+                        <el-button>2017-05-06</el-button>
+                    </el-col>
             </el-row>
             <h3>启动完成</h3>
             <router-link to="/index/plan_list"><el-button type="primary">返回方案列表</el-button></router-link>
