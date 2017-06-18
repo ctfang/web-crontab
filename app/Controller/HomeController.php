3704f4a721127117ee79ea0a9e0f9e968a4c19fb
selden1992 <2206582181@qq.com>
2017-06-18 21:45:34 +0800

    获取版本历史

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -23,7 +23,7 @@ class HomeController
     {
         $data = Crontab::getRestartInfo();
         if( !$data ){
-            return Output::error('没有重启信息',10000);
+            //return Output::error('没有重启信息',10000);
         }
         return Output::success('获取上次启用信息-时间和标示','10001',[
             'last_id'=>date("Hi"),
